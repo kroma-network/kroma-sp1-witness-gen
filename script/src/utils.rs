44135dee7bd0d13c32ad8d9bb6ae8beb27275c30
dongchangYoo <yoodc8167@gmail.com>
2024-09-27 18:35:05 +0900

    refac(script): update running cmd

---
+++ script/src/utils.rs
@@ -0,0 +1,32 @@
+use std::{path::PathBuf, str::FromStr};
+
+use op_succinct_host_utils::{fetcher::OPSuccinctDataFetcher, stats::ExecutionStats};
+use sp1_sdk::{block_on, ExecutionReport};
+
+const REPORT_DIR: &str = "execution-reports";
+
+pub fn report_execution(
+    data_fetcher: &OPSuccinctDataFetcher,
+    report: &ExecutionReport,
+    execution_duration: std::time::Duration,
+    l2_chain_id: u64,
+    l2_number: u64,
+) {
+    let mut stats = ExecutionStats::default();
+    block_on(async { stats.add_block_data(&data_fetcher, l2_number, l2_number).await });
+    stats.add_report_data(&report, execution_duration);
+    stats.add_aggregate_data();
+    println!("Execution Stats: \n{:?}", stats);
+
+    let mut report_path = PathBuf::from_str(REPORT_DIR).unwrap();
+    report_path.push(l2_chain_id.to_string());
+    if !std::path::Path::new(&report_path).exists() {
+        std::fs::create_dir_all(&report_path).unwrap();
+    }
+    report_path.push(format!("{}.csv", l2_number));
+
+    // Write to CSV.
+    let mut csv_writer = csv::Writer::from_path(report_path).unwrap();
+    csv_writer.serialize(&stats).unwrap();
+    csv_writer.flush().unwrap();
+}
