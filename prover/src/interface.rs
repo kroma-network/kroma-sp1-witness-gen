7397cba0c19b7e293b3c47c166be899a5d121bea
dongchangYoo <yoodc8167@gmail.com>
2024-10-24 13:27:40 +0900

    feat(prover): simplify prover

--- prover/src/interface.rs
+++ prover/src/interface.rs
@@ -73,46 +73,32 @@ impl Rpc for RpcImpl {
                 ProverError::invalid_input_hash(e.to_string()).to_json_error()
             })?;
 
-        // If this request has been made before, the `request_prove` method will terminate here.
-        let guard = self.task_lock.read().unwrap();
-        if let Some(request_id) = self.proof_db.get_request_id(&l2_hash, &l1_head_hash) {
-            match self.proof_db.get_proof_by_id(&request_id) {
-                Some(_) => {
-                    // The request is already completed.
-                    tracing::info!(
-                        "The request is already completed: {:?}, {:?}",
-                        user_req_id,
-                        request_id
-                    );
-                    return Ok(RequestResult::Completed);
-                }
-                None => {
-                    // The request is in processing.
-                    tracing::info!(
-                        "The request is in processing: {:?}, {:?}",
-                        user_req_id,
-                        request_id
-                    );
-                    return Ok(RequestResult::Processing);
-                }
-            }
-        }
-        drop(guard);
-
-        // Send a request to SP1 network.
+        // Check a status of the request.
         let guard = self.task_lock.write().unwrap();
+        let req_status = crate::utils::get_status_by_local_id(
+            &self.client,
+            &self.proof_db,
+            &l2_hash,
+            &l1_head_hash,
+        );
+
+        // Return the status in case of `Processing` or `Completed`.
+        if req_status == RequestResult::Processing || req_status == RequestResult::Completed {
+            tracing::info!("The {:?} status: {:?}", user_req_id, req_status);
+            return Ok(req_status);
+        }
+
+        // Send a request to the SP1 Network Prover only if the status is `None` or `Failed`.
         let request_id =
             crate::utils::request_prove_to_sp1(&self.client, witness).map_err(|e| {
                 tracing::error!("Failed to send request to SP1 network: {:?}", e);
                 ProverError::sp1_network_error(e.to_string()).to_json_error()
             })?;
-        tracing::info!("Sent request to SP1 network: {:?}", request_id);
+        tracing::info!("Sent request to SP1 network: {:?}, {:?}", user_req_id, request_id);
 
         // Store the `request_id` to the database.
-        self.proof_db.set_request_id(&l2_hash, &l1_head_hash, &request_id).map_err(|e| {
-            tracing::info!("The database is full: {:?}", e.to_string());
-            ProverError::db_error(e.to_string()).to_json_error()
-        })?;
+        self.proof_db.set_request_id(&l2_hash, &l1_head_hash, &request_id).unwrap();
+        tracing::info!("Stored request id to the database: {:?}, {:?}", user_req_id, request_id);
         drop(guard);
 
         Ok(RequestResult::Processing)
@@ -129,21 +115,14 @@ impl Rpc for RpcImpl {
                 ProverError::invalid_input_hash(e.to_string()).to_json_error()
             })?;
 
-        // Check if it has been requested.
-        let guard = self.task_lock.read().unwrap();
-        let request_id = match self.proof_db.get_request_id(&l2_hash, &l1_head_hash) {
-            Some(id) => id,
-            None => {
-                tracing::info!("The request is not found: {:?}", user_req_id);
-                return Ok(ProofResult::none());
-            }
-        };
-
         // Check if the proof is already stored.
-        if let Some(proof) = self.proof_db.get_proof(&l2_hash, &l1_head_hash) {
-            tracing::info!("The proof is already stored: {:?}, {:?}", user_req_id, request_id);
+        let guard = self.task_lock.read().unwrap();
+        if let Some(proof) =
+            crate::utils::get_proof_by_local_id(&self.proof_db, &l2_hash, &l1_head_hash)
+        {
+            tracing::info!("The proof is already stored: {:?}", user_req_id);
             return Ok(ProofResult::new(
-                &request_id,
+                &self.proof_db.get_request_id(&l2_hash, &l1_head_hash).unwrap(),
                 RequestResult::Completed,
                 proof.public_values.raw(),
                 proof.raw(),
@@ -151,41 +130,30 @@ impl Rpc for RpcImpl {
         }
         drop(guard);
 
-        // Check if the proof is already being generated in SP1 network.
-        let request_result =
-            crate::utils::get_proof_status_from_sp1(&self.client, &self.proof_db, &request_id)
-                .map_err(|e| {
-                    tracing::error!(
-                        "Failed to get proof status from SP1 network: {:?}, {:?}",
-                        user_req_id,
-                        request_id
-                    );
-                    ProverError::sp1_network_error(e.to_string()).to_json_error()
-                })?;
-
-        match request_result {
+        // Check if it has been requested.
+        let guard = self.task_lock.read().unwrap();
+        let proof_result = match crate::utils::get_status_by_local_id(
+            &self.client,
+            &self.proof_db,
+            &l2_hash,
+            &l1_head_hash,
+        ) {
             RequestResult::Completed => {
-                let _guard = self.task_lock.read().unwrap();
-                let proof = self.proof_db.get_proof_by_id(&request_id).unwrap();
-                Ok(ProofResult::new(
-                    &request_id,
+                let proof = self.proof_db.get_proof(&l2_hash, &l1_head_hash).unwrap();
+                ProofResult::new(
+                    &user_req_id,
                     RequestResult::Completed,
                     proof.public_values.raw(),
-                    format!("0x{}", proof.raw()),
-                ))
+                    format!("0x{:?}", proof.raw()),
+                )
             }
-            RequestResult::Processing => {
-                tracing::info!("The proof is in processing: {:?}, {:?}", user_req_id, request_id);
-                Ok(ProofResult::processing(request_id))
-            }
-            RequestResult::None => {
-                tracing::info!("The request is not found: {:?}, {:?}", user_req_id, request_id);
-                Ok(ProofResult::none())
-            }
-            RequestResult::Failed(msg) => {
-                tracing::info!("The request has been failed: {:?}", msg);
-                Ok(ProofResult::failed(request_id, msg))
-            }
-        }
+            RequestResult::Processing => ProofResult::processing(user_req_id),
+            RequestResult::None => ProofResult::none(),
+            RequestResult::Failed(msg) => ProofResult::failed(user_req_id, msg),
+        };
+        drop(guard);
+        tracing::info!("return the proof result: {:?}", proof_result);
+
+        Ok(proof_result)
     }
 }
--- prover/src/utils.rs
+++ prover/src/utils.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
-
+use alloy_primitives::B256;
 use anyhow::Result;
 use kroma_utils::deps_version::SP1_SDK_VERSION;
 use kroma_witnessgen::get_witness_impl::WitnessResult;
 use sp1_sdk::network::client::NetworkClient;
 use sp1_sdk::proto::network::{ProofMode, ProofStatus};
 use sp1_sdk::{block_on, SP1ProofWithPublicValues, SP1Stdin};
+use std::sync::Arc;
 
 use crate::proof_db::ProofDB;
 use crate::types::{RequestResult, SINGLE_BLOCK_ELF};
@@ -22,30 +22,55 @@ pub fn request_prove_to_sp1(client: &Arc<NetworkClient>, witness: String) -> Res
     Ok(request_id)
 }
 
-pub fn get_proof_status_from_sp1(
+pub fn get_status_by_local_id(
+    client: &Arc<NetworkClient>,
+    proof_db: &Arc<ProofDB>,
+    l2_hash: &B256,
+    l1_head_hash: &B256,
+) -> RequestResult {
+    let request_id = proof_db.get_request_id(&l2_hash, &l1_head_hash);
+    match request_id {
+        Some(id) => get_status_by_remote_id(client, proof_db, &id),
+        None => RequestResult::None,
+    }
+}
+
+pub fn get_status_by_remote_id(
     client: &Arc<NetworkClient>,
     proof_db: &Arc<ProofDB>,
     request_id: &str,
-) -> Result<RequestResult> {
+) -> RequestResult {
     match block_on(async { client.get_proof_status::<SP1ProofWithPublicValues>(request_id).await })
     {
         Ok((response, maybe_proof)) => match response.status() {
             ProofStatus::ProofFulfilled => {
-                proof_db.set_proof(&request_id, &maybe_proof.unwrap())?;
-                Ok(RequestResult::Completed)
+                proof_db.set_proof(&request_id, &maybe_proof.unwrap()).unwrap();
+                RequestResult::Completed
             }
             ProofStatus::ProofPreparing
             | ProofStatus::ProofRequested
-            | ProofStatus::ProofClaimed => Ok(RequestResult::Processing),
+            | ProofStatus::ProofClaimed => RequestResult::Processing,
             ProofStatus::ProofUnclaimed => {
-                Ok(RequestResult::Failed(response.unclaim_description.unwrap()))
+                RequestResult::Failed(response.unclaim_description.unwrap())
             }
             ProofStatus::ProofUnspecifiedStatus => {
                 tracing::error!("The proof status is unspecified: {:?}", request_id);
-                Ok(RequestResult::None)
+                RequestResult::None
             }
         },
         // There is only one error case: "Failed to get proof status"
-        Err(_) => Ok(RequestResult::None),
+        Err(_) => RequestResult::None,
+    }
+}
+
+pub fn get_proof_by_local_id(
+    proof_db: &Arc<ProofDB>,
+    l2_hash: &B256,
+    l1_head_hash: &B256,
+) -> Option<SP1ProofWithPublicValues> {
+    let request_id = proof_db.get_request_id(&l2_hash, &l1_head_hash);
+    match request_id {
+        Some(id) => proof_db.get_proof_by_id(&id),
+        None => None,
     }
 }
